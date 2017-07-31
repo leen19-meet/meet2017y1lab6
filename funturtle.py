69985d4a1060f9cf30b755e9eb7f1230b23bd8f3
leen19-meet <leen19@meet.mit.edu>
2017-07-31 15:40:13 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,35 @@
+import turtle
+
+##
+##leen= turtle.clone()
+##turtle.shape("square")
+##leen.shape("circle")
+##
+##leen.goto(100,100)
+##turtle.mainloop()
+
+##square = turtle.clone()
+##
+##square.shape('square')
+##square.goto(100,0)
+##square.goto(100,100)
+##square.goto(0,100)
+##square.goto(0,0)
+##square.mainloop()
+
+##turtle.shape('turtle')
+##square= turtle.clone()
+##square.shape('square')
+##square.goto(100,100)
+##
+##square.goto(300,300)
+##square.stamp()
+##square.goto(100,100)
+##turtle.mainloop()
+
+UP= 0
+LEFT= 1
+DOWN= 2
+RIGHT= 3
+directions= up
+
