a6284840e855fb33209e74915316e5a94fd426e0
leen19-meet <leen19@meet.mit.edu>
2017-07-31 18:52:23 +0300

    Stopping here

--- funturtle.py
+++ funturtle.py
@@ -1,3 +1,48 @@
+def up():
+    global direction
+    direction = UP
+    print("you pressed UP")
+    old_pos= turtle.pos()
+    x= old_pos[0]
+    y= old_pos[1]
+    turtle.goto(x, y+10)
+    print(turtle.pos())
+def down():
+    global direction
+    direction = DOWN
+    print("you pressed down")
+    old_pos= turtle.pos()
+    x= old_pos[0]
+    y= old_pos[1]
+    turtle.goto(x, y-10)
+    print(turtle.pos())
+def left():
+    global direction
+    direction = LEFT
+    print("you pressed left")
+    old_pos= turtle.pos()
+    x= old_pos[0]
+    y= old_pos[1]
+    turtle.goto(x-10, y)
+    print(turtle.pos())
+def right():
+    global direction
+    direction = RIGHT
+    print("you pressed right")
+    old_pos= turtle.pos()
+    x= old_pos[0]
+    y= old_pos[1]
+    turtle.goto(x+10, y)
+    print(turtle.pos())
+def spaces():S
+    if turtle.isdown():
+        turtle.penup()
+    else:
+        turtle.pendown()
+
+
+################################################333
+
 import turtle
 
 ##
@@ -32,19 +77,19 @@ LEFT= 1
 DOWN= 2
 RIGHT= 3
 
+UP_ARROW = "Up"
+DOWN_ARROW = "Down"
+LEFT_ARROW = "Left"
+RIGHT_ARROW = "Right"
+SPACEBAR = "space"
+
 direction = UP
 
-def up():
-    global direction
-    direction = UP
-    print("you pressed UP")
-def down():
-    global direction
-    direction = DOWN
-    print("you pressed down")
-def left():
-    global direction
-    direction = LEFT
-def right():
-    global direction
-    direction = RIGHT
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(left, LEFT_ARROW)
+turtle.onkeypress(right, RIGHT_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(spaces, SPACEBAR)
+turtle.listen()
+
+turtle.mainloop()
